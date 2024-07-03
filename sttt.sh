12f50960fb56314795853672ff275999e7deb6d7
Gump9510 <32225020+Gump9510@users.noreply.github.com>
2024-07-03 20:56:18 +0800

    Update sttt.sh

--- sttt.sh
+++ sttt.sh
@@ -1,1 +1,1 @@
-./io_net_launch_binary_linux --device_id=43cbc653-a183-458d-8b16-e7e2966fb66e --user_id=2d1e9512-1cc5-4aa3-9ccf-096329f3919f --operating_system="Linux" --usegpus=true --device_name="New-device"
+./io_net_launch_binary_linux --device_id=2caf3290-ea86-4c8a-9e55-36ebb8ef7e92 --user_id=2d1e9512-1cc5-4aa3-9ccf-096329f3919f --operating_system="Linux" --usegpus=true --device_name="New-device"
